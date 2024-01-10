cb1e360f1b033965017a8452e4b634577c9ebbfb
HamzaARAB4349 <arabhamza4349@gmail.com>
2024-01-10 15:05:48 +0100

    Abdallah work

--- elasticsearchApp/urls.py
+++ elasticsearchApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import  path
-from .views import add_article
+from .views import add_article,download_pdf
 
 
 
 urlpatterns = [
     path("add_article/",add_article,name='add_article'),
+    path("<path:url>/", download_pdf, name="download_pdf"),
 ]
--- elasticsearchApp/views.py
+++ elasticsearchApp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from .utils import (
+    download_pdf_from_url,
+    process_pdf_file,
+)  # Assume you put the function in a file named utils.py
+import os
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from elasticsearch_dsl import connections, Document, Text, Keyword
@@ -6,6 +13,39 @@ from .search_indexes import ArticleIndex
 
 # Define your Elasticsearch connection
 connections.create_connection(hosts=['http://localhost:9200'])
+
+
+def download_pdf(request, url):
+    file_name = download_pdf_from_url(url)
+
+    if file_name:
+        # Do something with the downloaded file, like serving it as a response or further processing
+        metadata = process_pdf_file(file_name)
+        try:
+            os.remove(file_name)
+        except OSError as e:
+            print(f"Error deleting file: {e}")
+
+        if metadata:
+            # You can customize the response format as needed
+            response_data = {
+                "title": metadata["title"],
+                "authors": metadata["authors"],
+                "institutions": metadata["institutions"],
+                "keywords": metadata["keywords"],
+                "abstract": metadata["abstract"],
+                "text": metadata["text"],
+                "references": metadata["references"],
+            }
+
+            # Return the metadata as a JSON response
+            return JsonResponse(response_data)
+        else:
+            return HttpResponse("Failed to extract metadata.", status=500)
+
+    else:
+        return HttpResponse("Failed to download the file.", status=500)
+
 
 @api_view(['POST'])
 def add_article(request):
