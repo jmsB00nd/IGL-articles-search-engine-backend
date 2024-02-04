d77a2050e833fba8f1e6156d1deda8c59d0bfec8
ELHart05 <o.allaoua@esi-sba.dz>
2024-02-04 13:58:06 +0100

    fixes

--- elasticsearchApp/views.py
+++ elasticsearchApp/views.py
@@ -37,6 +37,7 @@ def get_data_elasticsearch(request):
             "authors": list(hit.authors),
             "keywords": list(hit.keywords),
             "institutions": list(hit.institutions),
+            "resume": hit.resume
         })
 
     # Return the data as a JSON response
